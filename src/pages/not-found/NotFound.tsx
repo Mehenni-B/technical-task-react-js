e2717e5cbf6e075bd53efd9069b74172927a83a2
mehennib <mehenni.badreddine1@gmail.com>
2024-03-22 09:39:52 +0100

    Add not found page

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,8 +1,18 @@
+import { Heading, Image, Box, Text } from "@chakra-ui/react"
+import notFound from '../../assets/images/not_found.webp'
+import { Link } from 'react-router-dom'
+
 const NotFound: React.FC = () => {
     return (
-        <div>
-            <h1>404 Not Found</h1>
-        </div>
+        <>
+            <Box boxSize='xl' mx="auto" my={'8'} >
+                <Image src={notFound} />
+            </Box>
+            <Heading as="h1" size="2xl" mt="-48" textAlign={"center"} fontFamily={"poppins"} fontWeight={"medium"}>Not Found 404</Heading>
+            <Text textAlign={"center"} fontSize={"xl"} color="#0F82EA" mt="4" pb={'16'} textTransform={"capitalize"} fontWeight={"medium"}>
+                <Link to="/">Go back</Link>
+            </Text>
+        </>
     )
 }
 
